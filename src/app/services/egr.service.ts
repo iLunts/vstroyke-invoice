ff4695ca2b4b52e300801a9b919c22edd796ca87
Roman Luntsevich <shipik2008@gmail.com>
2020-03-30 17:43:02 +0300

    change EGR Api link

--- src/app/services/egr.service.ts
+++ src/app/services/egr.service.ts
@@ -5,7 +5,8 @@ import { HttpClient, HttpParams } from '@angular/common/http';
   providedIn: 'root'
 })
 export class EgrService {
-  baseUrl = 'http://egr.gov.by/egrn/API.jsp';
+  // baseUrl = 'http://egr.gov.by/egrn/API.jsp';
+  baseUrl = '//egr.gov.by/egrn/API.jsp';
 
   constructor(
     private _http: HttpClient,
